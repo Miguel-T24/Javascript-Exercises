804a144b51ac9c5b2d7141a6ab4e4b13f41f59c9
Miguel-T24 <miguelt2296@gmail.com>
2023-02-24 12:44:38 -0500

    Add Exercise #10, DOM Manipulation

--- Basics/10. multiplyDivideTwoNumbers.js
+++ Basics/10. multiplyDivideTwoNumbers.js
@@ -1,3 +1,18 @@
 'use strict'
 
 // 10. Write a Javascript Program to calculate multiplication and division of two numbers (input from user)
+
+let firstNumber = document.getElementById('n1');
+let secondNumber = document.getElementById('n2');
+const btnMultiply = document.getElementById('multiply-btn');
+const btnDivide = document.getElementById('divide-btn');
+let result = document.getElementById('value');
+
+btnMultiply.addEventListener('click', () =>{
+    let equal = parseInt(firstNumber.value) * parseInt(secondNumber.value)
+    result.textContent = equal
+})
+btnDivide.addEventListener('click', () =>{
+    let equal = parseInt(firstNumber.value) / parseInt(secondNumber.value)
+    result.textContent = equal
+})
