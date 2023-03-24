4f7a02d1e93e6aefe8cae2ec61085b9beee7ba10
Miguel-T24 <miguelt2296@gmail.com>
2023-03-24 18:09:21 -0500

    Add from 6 to 13 recursion javascript practice

---
+++ 4. Recursion/10. palindrome.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// 10. A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as the words madam or racecar, the date/time stamps 11/11/11 11:11 and 02/02/2020, and the sentence: "A man, a plan, a canal – Panama". Write a JavaScript program to check whether a given string is Palindrome or not using recursion.
+
+const test = (text) => {
+    if (typeof text !== 'string') {
+          return 'String should not be empty!'
+    }
+  
+    if (text.length <= 1) {
+      return true
+    }
+  
+    if (text[0] !== text[text.length - 1])
+    {
+      return false
+    } 
+    else 
+    {
+      return test(text.slice(1, text.length - 1))
+    }
+  }
+  console.log(test("madam"))
+  console.log(test("abdb"))
+  console.log(test("ab"))
+  console.log(test("a"))
