2a73a27edd9c3fc7c7ed3a79a42819b983747a96
Miguel-T24 <miguelt2296@gmail.com>
2023-03-28 07:54:00 -0500

    Add from 6 to 10 javascript object practice

---
+++ 10. Objects/7. subsetOfString.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// 7. Write a javascipr tprogram which return a subset of a string
+
+String.prototype.sub_String = function() 
+{
+  var subset = [];
+  for (var m = 0; m < this.length; m++) 
+  {
+    for (var n = m+1; n<this.length+1; n++) 
+    {
+      subset.push(this.slice(m,n));
+    }
+  }
+  return subset;
+};
+
+console.log("dog".sub_String());
---
+++ 10. Objects/9. calculateareaAndPerimeterOfcircle.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// 9. Write a javascirpt program to calculate the area and perimeter of a circle.
+
+function circle(radius)
+{
+    this.radius = radius;
+  // area method
+    this.area = function () 
+    {
+        return Math.PI * this.radius * this.radius;
+    };
+  // perimeter method
+    this.perimeter = function ()
+    {
+        return 2*Math.PI*this.radius;
+    };
+}
+var c = new circle(3);
+console.log('Area =', c.area().toFixed(2));
+console.log('perimeter =', c.perimeter().toFixed(2));
